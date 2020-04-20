33629a6e76ba1228596ee9584dedaafa5599d3f1
oleksmaistrenko <749978+oleksmaistrenko@users.noreply.github.com>
2020-04-20 17:54:36 +0300

    clean up of the lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,7 @@
+# import requests
 from botocore.vendored import requests
 from lxml import html
-import datetime
-
-club_id = "***"
-email = "***"
-password = "***"
+import datetime, os
 
 def parse_activity_html(activity_html):
     '''
@@ -23,15 +20,15 @@ def parse_activity_html(activity_html):
         activity_date = activity.xpath(".//div[@class='entry-head']/time")[0].text.strip()
         activity_date_day, activity_date_time, *_ = activity_date.split(" at ")
         # correct the dates
-        if activity_date_day == "Today":
-            activity_date_day = datetime.date.today().strftime("%B %-d, %Y")
-        elif activity_date_day == "Yesterday":
-            activity_date_day = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%B %-d, %Y")
+        if activity_date_day == 'Today':
+            activity_date_day = datetime.date.today().strftime('%B %-d, %Y')
+        elif activity_date_day == 'Yesterday':
+            activity_date_day = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%B %-d, %Y')
         activity_distance = activity.xpath(".//li[@title='Distance']")[0].text.strip()
         try:
-            activity_elevation_gain = activity.xpath(".//li[@title='Elev Gain']")[0].text.strip().replace(",", "")
+            activity_elevation_gain = activity.xpath(".//li[@title='Elev Gain']")[0].text.strip().replace(',', '')
         except IndexError:
-            activity_elevation_gain = ""
+            activity_elevation_gain = ''
         print(f'"{activity_athlete}","{activity_date_day}","{activity_date_time}","{activity_distance}","{activity_elevation_gain}"')
         records.append((activity_athlete, activity_date_day, activity_date_time, activity_distance, activity_elevation_gain))
         last_timestamp = activity_timestamp
@@ -41,29 +38,26 @@ def parse_activity_html(activity_html):
 
 
 def lambda_handler(event, context):
+    # get the club id
+    club_id = os.environ['CLUB_ID']
+    
+    # open session
     session_requests = requests.session()
 
     # get a token for this session
-    login_url = "https://www.strava.com/login"
+    login_url = 'https://www.strava.com/login'
     result = session_requests.get(login_url)
     tree = html.fromstring(result.text)
     authenticity_token = list(set(tree.xpath("//*[@id='login_form']/input[@name='authenticity_token']/@value")))[0]
 
-    payload = {
-        "email": email, 
-        "password": password, 
-        "authenticity_token": authenticity_token
-    }
-
     # login
     login_result = session_requests.post(
         'https://www.strava.com/session', 
-        data = payload, 
+        data = dict(email=os.environ['EMAIL'], password=os.environ['PASSWORD'], authenticity_token=authenticity_token), 
         headers = dict(referer=login_url)
     )
 
     # get the current month
-    current_month = datetime.datetime.now().strftime("%B")
     records = []
     
     # get the recent activity
@@ -74,7 +68,7 @@ def lambda_handler(event, context):
     # load more until we can
     while len(temp_records) > 0:
         club_recent_activity_continued = session_requests.get(f"https://www.strava.com/clubs/{club_id}/feed",
-            params = dict(feed_type="club", before=last_timestamp, cursor=last_timestamp + ".0")
+            params = dict(feed_type='club', before=last_timestamp, cursor=last_timestamp + ".0")
         )
         last_timestamp, last_month_name, temp_records = parse_activity_html(club_recent_activity_continued)
         records += temp_records
@@ -82,3 +76,4 @@ def lambda_handler(event, context):
     session_requests.close()
 
     return {'content' : records}
+    
